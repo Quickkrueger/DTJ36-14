b5a0c6667312915ff9c8e9b0e2d5d2b16508a3ae
Marshall Krueger <mgkrueger4@gmail.com>
2025-05-25 22:26:20 +0100

    Character things and all that

--- Assets/Scripts/PlayerScripts/BeeSuit.cs
+++ Assets/Scripts/PlayerScripts/BeeSuit.cs
@@ -3,10 +3,26 @@ using UnityEngine;
 public class BeeSuit : MonoBehaviour
 {
     int suitLevel = 0;
+    public Renderer renderer;
+    public Material shirtMaterial;
+    public Material pantsMaterial;
+    public GameObject hat;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Upgrade()
     {
         suitLevel++;
+        if (suitLevel == 1)
+        {
+            renderer.material = shirtMaterial;
+        }
+        else if (suitLevel == 2)
+        {
+            renderer.material = pantsMaterial;
+        }
+        else if (suitLevel == 3)
+        {
+            hat.SetActive(true);
+        }
     }
 
     public int GetSuitlevel()
